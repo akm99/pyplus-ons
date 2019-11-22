c3997b0c7003859c3a60814291a2355c38b13bda
Kirk Byers <ktbyers@twb-tech.com>
2019-11-21 19:44:21 -0800

    Updates procs with wait

--- day4/concurrency/collateral/cpu_procs.py
+++ day4/concurrency/collateral/cpu_procs.py
@@ -1,7 +1,6 @@
 """
 Purpose of this example is to demonstrate multiple processes
-(Python instances) being spawned -- in this case up to the
-AMOUNT_OF_MATHS.
+(Python instances) being spawned.
 
 Use top or activity/system monitor to see multiple instances
 of Python being ran.
@@ -11,24 +10,26 @@ import math
 import random
 
 
-AMOUNT_OF_MATHS = 8
+AMOUNT_OF_MATHS = 20
 MAX_WORKERS = 8
 
 
 def do_maths():
-    for i in range(random.randint(10000000, 30000000)):
+    for i in range(random.randint(1000000, 3000000)):
         final_sqrt = math.sqrt(i)
     return final_sqrt
 
 
 def main():
     pool = ProcessPoolExecutor(max_workers=MAX_WORKERS)
-    procs = []
+    futures = []
     for _ in range(AMOUNT_OF_MATHS):
-        procs.append(pool.submit(do_maths))
-    wait(procs)
-    for proc in procs:
-        print(proc.result())
+        futures.append(pool.submit(do_maths))
+
+    wait(futures)
+
+    for task in futures:
+        print(task.result())
 
 
 if __name__ == "__main__":
